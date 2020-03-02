991fb1d0b0afdfe2ceeb2ff980947d7150177f5e
Frank Selhorst <info@frankselhorst.nl>
2020-03-02 17:30:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import bitcoin from "./bitcoin.svg";
+
+import "./App.css";
+
+// const GET_PRICES = gql`
+//   {
+//     getPrices {
+//       price
+//     }
+//   }
+// `;
+
+const getPrice = currency => gql`
+  {
+    getPrice(currency: "${currency}") {
+      price
+    }
+  }
+`;
+
+function PriceTicker({ currency }) {
+  const { loading, error, data } = useQuery(getPrice(currency));
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const price = data.getPrice.price;
+
+  return (
+    <h2>
+      {price.symbol}&nbsp;
+      {price.last}
+    </h2>
+  );
+}
+
+// function AllPrices() {
+//   const { loading, error, data } = useQuery(GET_PRICES);
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error :(</p>;
+
+//   const prices = data.getPrices.price;
+//   return Object.keys(prices).map(price => {
+//     const currency = prices[price];
+//     return (
+//       <code key={`${currency.symbol}-${currency.last}`}>
+//         <pre>
+//           {currency.symbol} {currency.last}
+//           <br />
+//         </pre>
+//       </code>
+//     );
+//   });
+// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img alt="btc logo" src={bitcoin} />
+        <h1>Bitcoin GraphQL Price Ticker</h1>
       </header>
+      <PriceTicker currency={"EUR"} />
+      <PriceTicker currency={"USD"} />
+      {/* <AllPrices /> */}
     </div>
   );
 }
